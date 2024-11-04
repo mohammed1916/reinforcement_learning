5549099d74aab3368b87571eb6fd8a37ff50b065
mohammed1916 <arrahmaan9901@gmail.com>
2024-11-04 13:51:08 +0530

    Exam code to add Epsilon Greedy Bandit class for multi-armed bandit problem

---
+++ exam_epsilon_greedy.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class EpsilonGreedyBandit:
+    def __init__(self, n_arms, epsilon):
+        self.n_arms = n_arms
+        self.epsilon = epsilon
+        self.arm_counts = np.zeros(n_arms)
+        self.arm_values = np.zeros(n_arms)  
+    
+    def select_arm(self):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(self.n_arms)
+        else:
+            return np.argmax(self.arm_values)
+    
+    def update_estimates(self, chosen_arm, reward):
+        self.arm_counts[chosen_arm] += 1
+        
+        n = self.arm_counts[chosen_arm]
+        value = self.arm_values[chosen_arm]
+        self.arm_values[chosen_arm] = value + (reward - value) / n
+    
+    def run(self, n_steps):
+        total_reward = 0
+        rewards = np.zeros(n_steps)
+        
+        for step in range(n_steps):
+            chosen_arm = self.select_arm()
+            reward = np.random.randint(1,6)
+            self.update_estimates(chosen_arm, reward)
+            total_reward += reward
+            rewards[step] = reward
+        
+        return total_reward, rewards
+
+n_arms = 3                    
+epsilon = 0.1                
+n_steps = 7 
+
+bandit = EpsilonGreedyBandit(n_arms, epsilon)
+total_reward, rewards = bandit.run(n_steps)
+
+print(f"Total Reward: {total_reward}")
+print(f"Estimated Values of Arms: {bandit.arm_values}")
+print(f"Counts of Arms Pulled: {bandit.arm_counts}")
+
+x = np.arange(1, n_steps+1)
+plt.bar(x, rewards)
+plt.xlabel('Day')
+plt.ylabel('Reward')
+plt.title('Epsilon Greedy Approach: Distribution of Rewards')
+plt.yticks(np.arange(1, 6))
+plt.show()
